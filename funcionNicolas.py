3a49b6a90b3924215746de9183ed897296ef679c
Nick08020608 <nicolas.paulo.vega06@gmail.com>
2023-05-15 08:33:10 -0500

    ho

---
+++ funcionNicolas.py
@@ -0,0 +1,9 @@
+def information(name,lastname,age):
+    print(f"Name: {name}\n Lastname: {lastname}\n age: {age}")
+
+name= input("Name:")
+lastname= input("Lastname:")
+age = input("Age:")
+
+information(name,lastname,age)
+
