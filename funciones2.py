ee662737d8f124704d10e2f3c710c06e2554c84c
Pinzon5053 <129073102+Pinzon2006@users.noreply.github.com>
2023-05-15 07:58:48 -0500

    cambios 4

---
+++ funciones2.py
@@ -0,0 +1,10 @@
+#Funciones y Return
+#Despedir a un Usuario
+def despedida(nombres, tiempo):
+    print("message",variable)
+    print(f"message {variable} años")
+
+    variable = 
+    variable =
+    
+funcion = (variable)
