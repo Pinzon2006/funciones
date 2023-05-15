6540c4d008be279e0446cee291e9a7f7205028b4
Pinzon5053 <129073102+Pinzon2006@users.noreply.github.com>
2023-05-15 07:28:42 -0500

    Create funciones.py

---
+++ funciones.py
@@ -0,0 +1,15 @@
+#Funciones
+#Ejemplo Formulario
+#Nombre - Apellido - Edad - Ciudad - Correo - Años de Experiencia - Salario
+def formulario(nombres, apellidos, edad, ciudad, correo, years, salario):
+    print(f"\n\nLos Datos del Formulario enviado son:\nNombres: {nombres}\nApellidos: {apellidos}\nEdad: {edad}\nCiudad: {ciudad}\nCorreo: {correo}\nAños de Experiencia: {years}\nSalario: {salario}")
+    
+nombres = input("Ingrese su Nombre: ")
+apellidos = input("Ingrese sus Apellidos: ")
+edad = int(input("Ingrese su Edad: "))
+ciudad = input("Ingrese su Ciudad: ")
+correo = input("Ingrese su correo: ")
+years = int(input("Ingrese sus Años de Experiencia: "))
+salario = float(input("Ingrese su expectativa de Salario a ganar: "))
+
+formulario(nombres, apellidos,edad,ciudad,correo,years,salario)
