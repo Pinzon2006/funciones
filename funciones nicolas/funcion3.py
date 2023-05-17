cccfb5d2a4f052c12f7dd678a007f7f2baad08e8
Nick08020608 <nicolas.paulo.vega06@gmail.com>
2023-05-17 08:52:12 -0500

    j

--- funciones nicolas/funcion3.py
+++ funciones nicolas/funcion3.py
@@ -3,8 +3,8 @@ def multiplicar(x,y):
     multiplicaciony = y*y
     return multiplicacionx, multiplicaciony
 
-multiplicacionx= 1
-multiplicaciony= 2
+multiplicacionx= 1*1
+multiplicaciony= 2*2
 valordelamultiplicacion= multiplicacionx*multiplicaciony
 print(f"El valor de x es {multiplicacionx} y el vaolr de y es igual a {multiplicaciony} lo que no da el resultado {valordelamultiplicacion}")
 
